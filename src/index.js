65512578e4f7875cf8930360b52c40814b6c14cf
Vinícius Yoda <vyoda4604@gmail.com>
2023-02-26 09:44:42 -0300

    rs

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './global.css';
 import Home from './pages/Home';
-import { BrowserRouter, Router, Routes} from 'react-router-dom';
+import { BrowserRouter, Routes, Routes} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+
+      </Routes>
+      <Home />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
